21e86163b240ebb4e6078a32f32eccf1305cf68b
jmousewei <cwei1914@hotmail.com>
2014-07-31 15:00:02 +1200

    temp
    temp

--- LogEverything/Global.asax.cs
+++ LogEverything/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Serialization;  
 
 namespace LogEverything
 {
